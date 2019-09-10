45681be4de9c449bd2aba392e64ec68526b562e1
Victor Gonzalez <isc.victor.gonzalez@live.com.mx>
2019-09-10 10:45:12 -0500

    Refactor Time Entries controller

--- myredpy/controllers/time_entries.py
+++ myredpy/controllers/time_entries.py
@@ -25,34 +25,37 @@ class TimeEntries(Controller):
 
     def _default(self):
         """Default action if no sub-command is passed."""
-        user = self.app.redmine.user.get('current')
-        if user is None:
-            raise ValueError('Current user could not be found.')
+        try:
+            user = self.app.redmine.user.get('current')
+            if user is None:
+                raise ValueError('Current user could not be found.')
 
-        if self.app.pargs.period is not None:
-            period = self.app.pargs.period
-            if period == 'day':
-                self.get_entries_by_day(user.id)
-            if period == 'week':
-                self.get_entries_by_week(user.id, date.today(), self.app.pargs.show_weekends)
-            if period == 'last-week':
-                last_week_date = date.today() - timedelta(days=7)
-                self.get_entries_by_week(user.id, last_week_date, self.app.pargs.show_weekends)
-            if period == 'bi-week':
-                print('bi-week')
-            if period == 'month':
-                print('month')
-        else:
-            self.get_entries_by_day(user.id)
+            if self.app.pargs.period is not None:
+                period = self.app.pargs.period
+                if period == 'day':
+                    self.entries_by_day(user.id)
+                if period == 'week':
+                    self.entries_by_week(user.id, date.today(), self.app.pargs.show_weekends)
+                if period == 'last-week':
+                    last_week_date = date.today() - timedelta(days=7)
+                    self.entries_by_week(user.id, last_week_date, self.app.pargs.show_weekends)
+                if period == 'bi-week':
+                    print('bi-week')
+                if period == 'month':
+                    print('month')
+            else:
+                self.entries_by_day(user.id)
+        except Exception as e:
+            self.app.log.error(str(e), __name__)
 
     @ex(help='display time entry summary by period')
     def summary(self):
         self.app.render('summary')
 
-    def get_entries_by_day(self, user_id):
+    def entries_by_day(self, user_id):
         data = []
-        for p in self.get_projects():
-            entries = self.get_time_entries(user_id, p.id, date.today(), date.today())
+        for p in self.projects(self.ignored_projects()):
+            entries = self.time_entries(user_id, p.id, date.today(), date.today())
             data.append([p.id, p.name, sum(x.hours for x in entries)])
         # add totals row at the bottom
         total_hours = sum(x[2] for x in data)
@@ -66,7 +69,7 @@ class TimeEntries(Controller):
         if total_hours > 8:
             self.app.log.warning('You have logged more than 8 hours today.')
 
-    def get_entries_by_week(self, user_id, week_date=date.today(), show_weekends=False):
+    def entries_by_week(self, user_id, week_date=date.today(), show_weekends=False):
         print(datetime.now().strftime('%c'))
         monday = week_date - timedelta(days=week_date.weekday())
         # move to the previouse week if last-week options was sent.
@@ -76,8 +79,8 @@ class TimeEntries(Controller):
 
         data = []
         headers = ['project']
-        for p in self.get_projects():
-            entries = self.get_time_entries(user_id, p.id, monday, monday + timedelta(days=5))
+        for p in self.projects(self.ignored_projects()):
+            entries = self.time_entries(user_id, p.id, monday, monday + timedelta(days=5))
             days_to_check = range(0, days_to_show)
             entry_per_day = []
             day = monday
@@ -87,7 +90,7 @@ class TimeEntries(Controller):
                 headers.append('{}'.format(day.strftime('%a-%d.%m')))
                 day = day + timedelta(days=1)
 
-            project_entry = [self.get_text(p.name)]
+            project_entry = [self.format_project_name(p.name)]
             project_entry.extend(entry_per_day)
             data.append(project_entry)
 
@@ -100,8 +103,12 @@ class TimeEntries(Controller):
         self.app.render(data, headers=headers)
         self.app.log.info('Period Total Spent Time: {}'.format(sum(x for x in totals)))
 
-    def get_projects(self):
-        ignored_projects = list(map(int, self.app.config.get('myredpy', 'ignored_projects').split(',')))
+    def ignored_projects(self):
+        '''Returns the list of ignored projects configured by settings'''
+        return list(map(int, self.app.config.get('myredpy', 'ignored_projects').split(',')))
+
+    def projects(self, ignored_projects=[]):
+        '''Returns active projects for the current User. Additionally a list of ignored projects can be passed.'''
         result = []
         for p in self.app.redmine.project.all().filter(status=1):
             if p.id in ignored_projects:
@@ -109,12 +116,12 @@ class TimeEntries(Controller):
             result.append(p)
         return result
 
-    def get_time_entries(self, user_id, project_id, from_date, to_date):
+    def time_entries(self, user_id, project_id, from_date, to_date):
+        '''Returns the time entries filtered by user, project and date ranage'''
         return self.app.redmine.time_entry.filter(
             user_id=user_id, project_id=project_id, from_date=from_date, to_date=to_date
         )
 
-    def get_text(self, text):
-        # todo: replace this with a setting
-        # text = text.replace('', '')
-        return (text[: 40] + '..') if len(text) > 42 else text
+    def format_project_name(self, project_name):
+        '''Formats the given project name to a friendly format'''
+        return (project_name[: 40] + '..') if len(project_name) > 42 else project_name
